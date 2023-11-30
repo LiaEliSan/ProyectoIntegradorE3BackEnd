c07df8bd503a06ac4992a62bcaf2c3d2a848749a
Areli <areli.moreno213@gmail.com>
2023-11-29 18:32:05 -0600

    Se agrega springboot

---
+++ ecommerce/src/main/java/com/babydevs/ecommerce/BabyDevsApplication.java
@@ -0,0 +1,13 @@
+package com.babydevs.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyDevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyDevsApplication.class, args);
+	}
+
+}
---
+++ ecommerce/src/test/java/com/babydevs/ecommerce/BabyDevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babydevs.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyDevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
